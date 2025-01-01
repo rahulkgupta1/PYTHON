ec816bfc2e4b167cfe1511a1105723b59c8e37f3
rahulkgupta1 <rahulkgupta477@gmail.com>
2025-01-01 15:28:35 +0530

    Add files via upload

---
+++ 7_file/file.py
@@ -0,0 +1,35 @@
+#text: .txt, .docx, .log etc
+#Binary Files  : .mp4, .mov,.png,.jpeg etc
+
+# f = open("file/index.txt","r")
+# data = f.read()
+# data
+# print(type(data))
+# name_file = open("file/index.txt")
+# print(f"File name: {name_file.name}")
+# all_names   = name_file.read()
+# print(all_names)
+# name_file.close()
+
+# f = open("file/index.txt", "r")
+
+# data = f.read()
+# print(data)
+
+# line1 = f.readline()
+# print(line1)
+
+# line2 = f.readline()
+# print(line2)
+
+# f.close() 
+
+f = open("file/index.txt", "a")
+
+f.write("I want to learn JavaScript tomorow  ")
+
+f.close()
+ 
+
+# f = open("file/demo.txt", "a") # crate new file demo .txt 
+# f.close()
